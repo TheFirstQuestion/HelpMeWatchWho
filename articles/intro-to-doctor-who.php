d4924342cbbbdba2770c65f343c7b34956dfba29
Sophie Opferman <sophieopferman6@gmail.com>
2016-09-17 18:02:21 -0400

    continue intro to DW

--- articles/intro-to-doctor-who.php
+++ articles/intro-to-doctor-who.php
@@ -6,20 +6,18 @@
 
 <!--
 	
-	Talk about what the show is about in a big sense
-		Sci-fi, but not strictly -- romance, fairy tale
-	What episodes are like
-		some funny
-		some sad
+	*Talk about what the show is about in a big sense
+	*	Sci-fi, but not strictly -- romance, fairy tale
+	*What episodes are like
+	*	some funny
+	*	some sad
 	
-	Background/history
-	Classic vs. New, spin-offs
-	"the Doctor", not "Doctor Who"
-	Modern times prevalent
-	Companions leave/arrive
-	Regeneration/numbering
-	Plot arcs
-	Reused sets/actors (Meet self)
+	*Background/history
+	*Classic vs. New
+	*"the Doctor", not "Doctor Who"
+	*Modern times prevalent
+	*Companions leave/arrive
+	*Regeneration/numbering
 	
 	What happens when you watch the show
 	
@@ -39,9 +37,11 @@
 </div>
 
 
+
+
 <h2>What is <i>Doctor Who</i> About?</h2>
 
-<div class="imgWrapper fullSize"><img src="images/about.png" /></div>
+<div class="fullSize"><img src="images/about.png" /></div>
 
 <p><i>Doctor Who</i> is not a normal television show. It has two stories&#8212;each episode has its own plot and resolution and lessons learned and all those typical things. But there's also a deeper meaning, lessons and morals that apply to real life. <i>Doctor Who</i> isn't typical sci-fi in that it isn't full of violence and complicated tech-talk&#8212;the story revolves around the characters.</p>
 
@@ -71,9 +71,107 @@
 
 <div class="imgWrapper"><img src="images/1.gif" /><img src="images/2.gif" /></div>
 
+
+
+
+
+
+
+
 <br><br><br>
 
 <h2>The Complicated History of <i>Doctor Who</i></h2>
+
+<p><i>Doctor Who</i> is the longest running sci-fi show in the world. It first aired on November 23, 1963. William Hartnell played the Doctor, and he was accompanied by his granddaughter, Susan (Carol Ann Ford), and two of her schoolteachers, Ian Chesterton (William Russel) and Barbara Wright (Jacqueline Hill).</p>
+
+<div class="imgWrapper"><img src="images/quartet.jpg" /></div>
+
+<p>The show was initially very popular, despite the challenges it faced behind the scenes, but its populatiry was cemented after the second serial by the introduction of the Doctor's most famous enemy&#8212;the Daleks.</p>
+
+<div class="imgWrapper"><img src="images/daleks.jpg" /><img src="images/believed.jpg" /></div>
+
+<p>The show continued, old companions leaving and new ones arriving. However, in 1965, Hartnell's arteriosclerosis began to worsen, impacting his ability to remember lines (the show was filmed essentially live). The original production team also departed at this time, and Hartnell didn't get along with the new team. Everyone wanted the show to continue, but it was agreed that Hartnell needed to replaced, just as the many companions had been replaced. But how?</p>
+
+<div class="imgWrapper"><img src="images/hartnell.jpg" /></div>
+
+<p>The writers came up with the idea of "regeneration." When aliens like the Doctor are approaching death, they are able to remain alive by renweing themselves. In the process, they change completely, becoming basically a whole new person&#8212;played by another actor. This is how the show was able to continue without William Hartnell. And this is how the show has continued on for over 50 years.</p>
+
+<div class="imgWrapper"><img src="images/tenth planet 2.jpg" /><img src="images/tenth planet 3.jpg" /><img src="images/tenth planet 4.jpg" /></div>
+
+<p>Over the next 50 years, many people would play the Doctor. Officially, there are twelve Doctors. Within the fandom, they are commonly referred to by their numbers&#8212;William Hartnell's Doctor is One, Patrick Troughton's is Two, and so on. The current Doctor, played by Peter Capaldi, is Twelve.</p>
+
+<div class="imgWrapper"><img src="images/doctors.jpg" /></div>
+
+<p class="note"><b>Note:</b> There is some confusion about the numbering system due to events that occured during <a href="/list.php#NW4">Series 4</a> and <a href="/list.php#NW7">Series 7</a>. You may sometimes encounter references to "8.5" or "Ten and a half." Generally, however, the above numbering is used within the fandom.</p>
+
+<div class="imgWrapper"><img src="images/doctors 2.png" /></div>
+
+<p>Despite having a mechanic for continuing the show forever, the show's popularity eventually died down. In the 80's, the show lost its way due to behind the scenes drama. The Sixth Doctor was unpopular, and the BBC was facing budget cuts. The show became darker and more violent, and lot of people at the BBC began to hate it, including the Controller of BBC 1. The show was scheduled against <i>Coronation Street</i>, a very popular soap opera, and ratings dropped even more. In 1989, <i>Doctor Who</i> was cancelled, ending the classic series, or "Classic Who."</p>
+
+<div class="imgWrapper"><img src="images/survival.jpg" /><img src="images/somewhere out there.jpg" /></div>
+
+<p>Even though the show was over, the fans never gave up. In the 90's, <i>Doctor Who</i> continued in the form of novels, comedy sketches, audio dramas, and fan films.</p>
+
+<div class="imgWrapper"><img src="images/VNA novel.jpg" /><img src="images/dimensions in time.jpg" /><img src="images/audio drama.jpg" /></div>
+
+<p>By 1996, fans were pushing for a continuation of the show. An American company was interested, but only wanted to commit to a telemovie. It was hoped that <a href="/episodes.php?episodeID=500&seasonID=58"><i>Doctor Who: the Movie</i></a> could lead to the show being picked up and the production of an American reboot; thus, the movie picked up where the show had left off, which was alienating to new viewers. The movie was made, but the show was not continued. <i>Doctor Who</i> remained on haitus.</p>
+
+<div class="imgWrapper"><img src="images/tv movie.png" /><img src="images/dvd cover.jpg" /><img src="images/tvm.jpg" /></div>
+
+<p>Then, in 2005, <i>Doctor Who</i> returned at last, beginning the new series, or "New Who." It was a clean start, inviting to new viewers, and had a budget much larger than that of Classic Who. Most modern fans of <i>Doctor Who</i> have seen only the new series. If you want to start watching <i>Doctor Who</i>, this is where you should start.</p>
+
+<div class="imgWrapper"><img src="images/reboot.jpg" /><img src="images/logo.png" /><img src="images/season 1.jpg" /></div>
+
+
+
+
+
+
+
+<br><br><br>
+
+<h2>Starter Episodes</h2>
+
+<p>Even if you think you're ready to dive in, you might want to try a few episodes before committing to the show. The first episode isn't really the best example of how the great the show can be. We're got a few recomendations for beginning to watch.</p>
+
+<div class="fullSize"><a href="/episodes.php?episodeID=290&seasonID=29"><img src="images/blink.jpg" /></a></div>
+
+<a href="/episodes.php?episodeID=290&seasonID=29"><h2>Blink</h2></a>
+
+<p><b>Why You Should Watch It:</b> <i>Blink</i> is regarded by fans as one of the best episodes of <i>Doctor Who</i>. It's representative of the show in its focus on characters, time-travel craziness, and general creepiness. It's slightly unusual, however, in that the Doctor is barely in the episode. Overall, it's a very good episode, and if you like it, you'll probably like <i>Doctor Who</i>.</p>
+
+<p><b>What It's About:</b> When Sally Sparrow breaks into an old house, she's looking for the perfect picture. Instead, she finds some creepy statues and a mysterious message addressed to her from a man called the Doctor. It's from the year 1969, but he somehow seems able to predict the future. She finds a secret video, hidden on her DVDs, of the Doctor having half a conversation. He's in trouble, and the only person who can save him is her. The only the problem: the creepy statues aren't just statues.</p>
+
+
+
+<div class="fullSize"><a href="/episodes.php?episodeID=208&seasonID=22"><img src="images/unicorn and the wasp.jpg" /></a></div>
+
+<a href="/episodes.php?episodeID=208&seasonID=22"><h2>The Unicorn and the Wasp</h2></a>
+
+<p><b>Why You Should Watch It:</b> <i>The Unicorn and the Wasp</i> is one of the more lighthearted episodes of <i>Doctor Who</i>. It shows off the humor of the show, as well as the extent to which the writers love to interact with history. It's a typical historic episode, and gives you an idea of what to expect from the average episode.</p>
+
+<p><b>What It's About:</b> The Doctor and Donna arrive in the 1920's and happen upon Agatha Christie, the famous mystery writer. Soon after, murders start occuring. The whole thing is like one of her books&#8212;a man found mysteriously dead, a thief on the loose, and a long lost family member returning home&#8212;except for the giant wasp on the loose.</p>
+
+
+
+
+<div class="fullSize"><a href="/episodes.php?episodeID=162&seasonID=16"><img src="images/vincent and the doctor.jpg" /></a></div>
+
+<a href="/episodes.php?episodeID=162&seasonID=16"><h2>Vincent and the Doctor</h2></a>
+
+<p><b>Why You Should Watch It:</b> <i>Vincent and the Doctor</i> is another historical episode. It reveals some of the deeper messages in the show, and also gives you a sense of the Eleventh Doctor. It's got a very different feel to it than <i>The Unicorn and the Wasp</i> in that it's much more quiet and introspective; you get a deep look at characters in a way that doesn't really happen in an episode as lighhearted as <i>The Unicorn and the Wasp</i>.</p>
+
+<p><b>What It's About:</b> The Doctor and Amy are visiting an art museum when they find a monster hidden in one of Vincent Van Gough's paintings&#8212;they need to speak to him immediately! They take the TARDIS and happen upon him, becoming his friends and accompanying him on his painting trip. They fight the monster, but end up saving Vincent in a way they had never intended.</p>
+
+
+
+<div class="fullSize"><a href="/episodes.php?episodeID=25&seasonID=2"><img src="images/flatline.jpg" /></a></div>
+
+<a href="/episodes.php?episodeID=25&seasonID=2"><h2>Flatline</h2></a>
+
+<p><b>Why You Should Watch It:</b> <i>Flatline</i> will give you a peek into the darker side of <i>Doctor Who</i>. It's got funny moments, but the plot forces the Doctor and Clara to confront issues deeper and darker than terryifying monsters. You get a sense of Twelve; the way his past affects him and the way he tries to avoid it, and you get to see how a companion acts without the Doctor.</p>
+
+<p><b>What It's About:</b> The Doctor is trying to get Clara home, but they're forced to land when something goes wrong with the TARDIS. It shrunk! Something is stealing energy from their ship, and it's up to Clara to find out how to fix it when the Doctor is trapped in the tiny TARDIS.</p>
 
 
 
